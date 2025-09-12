ce27224eb6edcc46c33ac5273ff95161419e34e3
Pavelc4 <dimzzy23@gmail.com>
2025-09-12 16:57:11 +0700

    feat: enhance styling with DaisyUI and custom theme
    - Updated style.css to include DaisyUI plugin and defined a light theme with custom color variables.
    - Modified tailwind.config.js to extend the theme with a custom font family and added a new theme configuration for DaisyUI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
   plugins: [require("daisyui")],
   daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#15369B",
+          "primary-content": "#fff",
+        },
+      },
+      
+    ],
   },
 };
